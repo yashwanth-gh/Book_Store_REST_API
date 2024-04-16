4b011555af044725775d468a9d40cba55b6351fd
Yashwanth B M <yashwanthbm36@gmail.com>
2024-04-16 12:40:33 +0530

    Project config and setup done

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return <div className="app">
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      Test
+    </ThemeProvider>
+  </div>;
+}
+
+export default App;
